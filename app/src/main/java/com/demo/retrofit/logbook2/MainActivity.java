d8e273a78bc5a7fd5f2712aba349726c191142ad
LePhuongTrung <52908411+hanhehun1@users.noreply.github.com>
2022-11-16 23:31:58 +0700

    update Check Url Image

--- app/src/main/java/com/demo/retrofit/logbook2/MainActivity.java
+++ app/src/main/java/com/demo/retrofit/logbook2/MainActivity.java
@@ -3,38 +3,47 @@ package com.demo.retrofit.logbook2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView img;
     int check;
-    List<String> url;
+    ArrayList<String> url;
     TextInputLayout urlEnter;
+    TextView numberPage;
+
     int i = 0;
+    int current = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        urlEnter = findViewById(R.id.textInputLayout);
         img = findViewById(R.id.imageView);
+        numberPage = findViewById(R.id.textView);
+
         url = new ArrayList<String>();
         url.add("https://wallpaper.dog/large/5531074.png");
         url.add("https://wallpaper.dog/large/5531110.jpg");
@@ -50,16 +59,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void extracted() {
-        Glide.with(MainActivity.this)
+        Picasso.get()
                 .load(url.get(i))
-                .centerCrop()
-                .into(img);
+                .into(img, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Log.i("numberPage:", String.valueOf(i));
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        url.remove(url.get(i));
+                        i = current;
+                        Toast.makeText(MainActivity.this, "Url not return Img", Toast.LENGTH_SHORT).show();
+                        extracted();
+                    }
+                });
+        numberPage.setText((i+1)+"/"+url.size());
     }
 
     public void nextImg(View view){
         i++;
         if (i >=url.size())
             i = 0;
+        current = i;
         extracted();
     }
 
@@ -67,18 +90,18 @@ public class MainActivity extends AppCompatActivity {
         i--;
         if (i <0)
             i = url.size() -1;
+        current = i;
         extracted();
     }
     public void addLink(View view){
-        urlEnter = findViewById(R.id.textInputLayout);
-
-        url.add(urlEnter.getEditText().getText().toString());
+        String urlStr = urlEnter.getEditText().getText().toString();
+        url.add(urlStr);
         if (check < url.size()){
-            Toast.makeText(this, "Add Succeed", Toast.LENGTH_SHORT).show();
             i = url.size() -1;
             extracted();
         } else {
-            Toast.makeText(this, "Add Fail", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Add to ArrayList Fail", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
